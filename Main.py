e6981a24fdeb33cd498f0d9009ed09d5ade38754
Vianna2lee <vianna.lee@laetottenham.ac.uk>
2024-11-28 14:27:51 +0000

    finish

--- Main.py
+++ Main.py
@@ -32,6 +32,7 @@ class student(person):
             self.avg_grade=0
         else:
             self.avg_grade= sum(self.grades)/len(self.grades)
+  
 
     def get_student_summary(self):
         print(f"student ID: {self.student_id} Course: {self.course} Avg grade: {self.avg_grade}")
@@ -49,34 +50,54 @@ class professor(person):
         self.department=department
         self.salary=salary
 
-    def give_feedback(self,student,feedback):
-        print ("Feedback for {student.name}: {feedback}")
+    def give_feedback(self,Student,feedback):
+        print (f"Feedback for {Student.name}: {feedback}")
     
-    def increase_salary(self,percentage):
-        self.salary=self.salary*(1+percentage)
+    def increase_salary(self):
+        self.salary=self.salary*(1.1)
 
-    def get_professor 
-
-
-
+    def get_professor_summary(self):
+        print(f"Staff ID: {self.staff_id}  Department: {self.department}  Salary:{self.salary} ")
 
 
 
 
 
 
-Fathima=person("Fathima",18,"Male")
+class administrator (person):
+    def __init__(self,name,age,gender, admin_id,office,years_of_service):
+        super().__init__(name,age,gender)
+        self.admin_id=admin_id
+        self.office=office
+        self.years_of_service=years_of_service
+    
+    def set_admin_details(self, admin_id, office, years_of_service):
+        self.admin_id=admin_id
+        self.office=office
+        self.years_of_service=years_of_service
+
+    def increment_service_years(self):
+        self.years_of_service += 1
+
+    def get_admin_summary(self ):
+        print(f" Admin ID: {self.admin_id}  Office: {self.office}  Years of service: {self.years_of_service} ")
 
 
-Fathima= student("Fathima",18,"Male","S5462","CS")
-Fathima.set_details("Fathima",17,"Female")
+Professor=professor("Dr. Imran",99,"Female","Fathima1234", "Econ", 1000)
 
+Fathima = student("Fathima Williams", 3, "Female", "1am0thered", "CS")
 Fathima.add_grade(89)
-Fathima.add_grade(22)
-Fathima.add_grade(78)
-Fathima.add_grade(88)
-Fathima.add_grade(72)
-
+Fathima.add_grade(79)
+Fathima.add_grade(99)
 Fathima.calculate_average_grade()
-
 Fathima.get_student_summary()
+
+
+Professor.give_feedback(Fathima, "Great work on your assignment!")
+Professor.increase_salary()
+Professor.get_professor_summary()
+
+
+Marli=administrator ("Marli", 39, "Female", "TToTT", "U38, Teacher Block", 5)
+Marli.increment_service_years()
+Marli.get_admin_summary()
